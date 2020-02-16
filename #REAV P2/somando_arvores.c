66195245889fcb88d102ef22c0865c9c8e36bbc4
Gabriel Souza <glls@ic.ufal.br>
2020-02-16 17:48:59 -0300

    lul

--- #REAV P2/somando_arvores.c
+++ #REAV P2/somando_arvores.c
@@ -3,31 +3,54 @@
 #include <string.h>
 #include <math.h>
 
-struct binary_tree 
-{
-	int item;
-	struct binary_tree *left;
-	struct binary_tree *right;
+typedef struct binary_tree binary_tree;
+
+struct binary_tree {
+    int item;
+    int height;
+    binary_tree *left, *right;
 };
 
-struct binary_tree* create_empty_binary_tree()
-{
-	return NULL;
+int is_empty(binary_tree *bt) {
+    return bt == NULL;
 }
 
-struct binary_tree* create_binary_tree (int item, struct binary_tree *left, struct binary_tree *right)
-{
-	struct binary_tree *new_binary_tree = (struct binary_tree*) malloc(sizeof(struct binary_tree));
-	new_binary_tree->item = item;
-	new_binary_tree->left = left;
-	new_binary_tree->right = right;
-	return new_binary_tree;
+binary_tree *create_binary_tree(int item, binary_tree *left, binary_tree *right) {
+    binary_tree *bt = (binary_tree*) malloc(sizeof(binary_tree));
+    bt->item = item;
+    bt->left = bt->right = NULL;
+    return bt;
 }
 
-int is_empty (struct binary_tree *binary_tree)
-{
-	if (binary_tree == NULL) return 1;
-	else return 0;
+binary_tree* construct_binary_tree(char *str, int *pos) {
+
+    binary_tree *bt = NULL; //ponteiro para a arvore
+    int i = 0;
+    int numInt;       //numero do no (inteiro)
+    char numStr[50];  //numero do no (string)
+
+    while(str[*pos] == '(') { //ignora os parenteses abrindo
+        *pos += 1;
+    }
+
+    if(str[*pos] == ')') { //se encontrar parentese fechando, retorna null
+        *pos += 1;         // pois o ponteiro eh nulo
+        return bt;
+    }
+    else {
+        while(str[*pos] != '(') {    //se for diferente de abre parenteses
+            numStr[i++] = str[*pos]; //le o numero string
+            *pos += 1;
+        }
+        numStr[i] = '\0'; //finalizando a string com '/0'
+        numInt = atoi(numStr); //converte o numero string para inteiro
+
+        bt = create_binary_tree(numInt, NULL, NULL); //cria um no com o numero inteiro
+        bt->left = construct_binary_tree(str, pos);  //constroi a arvore para o no esquerdo
+        bt->right = construct_binary_tree(str, pos); //constroi a arvore para o no direito
+    }
+    *pos += 1;
+    return bt; //retorna o ponteiro para a arvore
 }
 
 struct binary_tree *check_sum(struct binary_tree *bt, int wanted, int sum, int *flag)
@@ -51,60 +74,29 @@ struct binary_tree *check_sum(struct binary_tree *bt, int wanted, int sum, int *
     check_sum(bt->right, wanted, sum, flag);
 }
 
-void print_pre_order_brackets(struct binary_tree *binary_tree)
-{
-	printf(" (");
-	if (!is_empty(binary_tree)) 
-	{
-		if (binary_tree != NULL) printf(" %d ", binary_tree->item);
-		else printf(" ()");
-		if (binary_tree->left == NULL) printf(" ()");
-		else print_pre_order_brackets(binary_tree->left);
-		printf(" ");
-		if (binary_tree->right == NULL) printf(" ()");
-	 	else print_pre_order_brackets(binary_tree->right);
-	}
-	printf(" )");
-}
+void print_pre_order(binary_tree *bt) {
+    if (!is_empty(bt)) {
+        printf("(");
+        printf("%d ", bt->item);
 
-struct binary_tree* create_binary_tree_brackets(struct binary_tree *binary_tree, char *input, int *i)
-{
-	if (input[(*i)++] == '(')
-	{
-		if (input[*i] == ')') {
-			(*i)++;
-			return NULL;	
-		} 
-		else
-		{
-			int j = 0, value = 0, node[100];
-			while (input[(*i)] != '(')
-			{
-				node[j] = input[*i];
-				j++;
-				(*i)++;
-			}
-			int size = j;
-			j = 0;
-			while (j != size) 
-			{
-				value += pow(10, size - j - 1) * (node[j] - '0');
-				j++;
-			}
-			binary_tree = create_binary_tree(value, NULL, NULL);
-			binary_tree->left = create_binary_tree_brackets(binary_tree->left, input, i);
-			binary_tree->right = create_binary_tree_brackets(binary_tree->right, input, i);
-		}
-		(*i)++;
-		return binary_tree;
-	}
+        if (bt->left == NULL) {
+            printf("()");
+        } else {
+            print_pre_order(bt->left);
+        } if (bt->right == NULL) {
+            printf("()");
+        } else {
+            print_pre_order(bt->right);
+        }
+        printf(")");
+    }
 }
 
 int main()
 {
     while(1)
     {
-        struct binary_tree *bt = create_empty_binary_tree();
+        struct binary_tree *bt = NULL;
 
         char stringArv[99999] = {0}, data;
         int i = 0, j = 0, k = 0, flag = 0, item;
@@ -120,7 +112,7 @@ int main()
         {
             scanf(" %c", &data);
 
-            if(data != ' ' && data != '\n')
+            if(data != ' ' && data != '\n' && data != '\0')
             {
                 stringArv[i] = data;
                 i++;
@@ -144,8 +136,7 @@ int main()
 
         stringArv[i] = '\0';
 
-        bt = create_binary_tree_brackets(bt, stringArv, &k);
-        //print_pre_order_brackets(bt);
+        bt = construct_binary_tree(stringArv, &k);
 
         check_sum(bt, item, 0, &flag);
 
